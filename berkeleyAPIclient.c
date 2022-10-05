a269e2d91d29921194c99579d69ac8198728deea
Nicky <nicoletteann.arriola@microchip.com>
2022-10-05 13:09:56 +0800

    updates client code

--- berkeleyAPIclient.c
+++ berkeleyAPIclient.c
@@ -14,12 +14,11 @@
 int main (int argc, char **argv)
 {
     int result;
-    int socketFd;               /* TCP/IP socket descriptor */
+    int socketFd;               
 
-    /* structures for use with getaddrinfo() */
-    struct addrinfo hints;      /* hints for getaddrinfo() */
-    struct addrinfo *servInfo;  /* list of info returned by getaddrinfo() */
-    struct addrinfo *p;         /* pointer for iterating list in servInfo */
+    struct addrinfo hints;      
+    struct addrinfo *servInfo;  
+    struct addrinfo *p;         
 
     /* argv[1] is host name, argv[2] is port number, make sure we have them */
     if (argc != 3)
@@ -32,13 +31,11 @@ int main (int argc, char **argv)
 
     memset(&hints, 0, sizeof(hints));
 
-    /* type of server we're looking for */
-    hints.ai_family = AF_INET;          /* internet address family */
-    hints.ai_socktype = SOCK_STREAM;    /* stream sock */
-    hints.ai_protocol = IPPROTO_TCP;    /* tcp/ip protocol */
-    hints.ai_flags = AI_CANONNAME;      /* include canonical name */
+    hints.ai_family = AF_INET;          
+    hints.ai_socktype = SOCK_STREAM;   
+    hints.ai_protocol = IPPROTO_TCP;  
+    hints.ai_flags = AI_CANONNAME;   
 
-    /* get a linked list of likely servers pointed to by servInfo */
     result = getaddrinfo(argv[1], argv[2], &hints, &servInfo);
 
     if (result != 0)
@@ -53,45 +50,34 @@ int main (int argc, char **argv)
 
     while (p != NULL)
     {
-        /* use current info to create a socket */
         socketFd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
 
         if (socketFd >= 0)
         {
-            /***************************************************************
-            * We got the socket we asked for try to connect.
-            *
-            * NOTE: connect() has an unspecified time out.  For a good
-            * a sample of connecting with a timeout see
-            * http://developerweb.net/viewtopic.php?id=3196 for
-            ***************************************************************/
             result = connect(socketFd, p->ai_addr, p->ai_addrlen);
 
             if (result != 0)
             {
-                /* this socket wouldn't except our connection */
                 close(socketFd);
             }
             else
             {
-                /* we're connected, get out of this loop */
                 break;
             }
         }
 
-        p = p->ai_next;     /* try next address */
+        p = p->ai_next;
     }
 
     if (NULL == p)
     {
-        /* we never found a server to connect to */
         fprintf(stderr, "Unable to connect to server.\n");
         freeaddrinfo(servInfo);
         exit(EXIT_FAILURE);
     }
 
     printf("Connected to %s\n", p->ai_canonname);
-    freeaddrinfo(servInfo);     /* we're done with this */
+    freeaddrinfo(servInfo); 
 
     /***********************************************************************
     * send messages to echo server and receive echos until user sends empty
